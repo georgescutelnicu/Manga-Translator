382544b3e1bb1d4f34f99ad54e7a3780e3353550
georgescutelnicu <george.v.scutelnicu@gmail.com>
2024-04-05 10:20:54 +0300

    Add tests for translator.py

---
+++ translator/test_translator.py
@@ -0,0 +1,28 @@
+import pytest
+from translator import MangaTranslator
+
+@pytest.fixture
+def translator():
+    return MangaTranslator()
+
+def test_translate_with_google(translator):
+    ja_text = "こんばんは！"
+    en_translation = "Good evening!"
+    translated_text = translator.translate(ja_text, method="google")
+    assert translated_text == en_translation
+
+def test_translate_with_hf(translator):
+    ja_text = "こんばんは！"
+    en_translation = "Good evening!"
+    translated_text = translator.translate(ja_text, method="hf")
+    assert translated_text == en_translation
+
+def test_invalid_translation_method(translator):
+    ja_text = "こんばんは！"
+
+    try:
+        translator.translate(ja_text, method="Mirai")
+    except ValueError as e:
+        assert str(e) == "Invalid translation method."
+    else:
+        assert False, "No exception was raised."
